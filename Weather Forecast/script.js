abb1e6fc7eb124c3f4928853a74f7ae989d6b09d
KeplerInCoding <shreyakv8@gmail.com>
2023-06-02 08:18:30 +0530

    .

--- Weather Forecast/script.js
+++ Weather Forecast/script.js
@@ -1,51 +1,78 @@
-const API_key = '547ce9cae097717245b375ca024431ed';
+const userWeather = document.querySelector('.userWeather');
+const searchWeather = document.querySelector('.searchWeather');
+let currentTab = userWeather;
+
+searchWeather.addEventListener('click', () => {
+    currentTab.classList.add('currentTab');
+    userWeather.classList.remove('currentTab');
+    currentTab = searchWeather;
+});
+
+userWeather.addEventListener('click', () => {
+    currentTab.classList.add('currentTab');
+    userWeather.classList.remove('currentTab');
+    currentTab = userWeather;
+});
 
 
-async function showWeather()
-{
-    try{
-        const city = 'goa';
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
-        const data = await response.json();
-        console.log("Wather data -> ", data);
 
-        let newPara=document.createElement('p');
-        newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+
+
+
+
+
+
+
+
+
+// const API_key = '547ce9cae097717245b375ca024431ed';
+
+
+// async function showWeather()
+// {
+//     try{
+//         const city = 'goa';
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
+//         const data = await response.json();
+//         console.log("Wather data -> ", data);
+
+//         let newPara=document.createElement('p');
+//         newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
         
-        document.body.appendChild(newPara);
-    }
-    catch{
-        console.log("Error");
-    }
+//         document.body.appendChild(newPara);
+//     }
+//     catch{
+//         console.log("Error");
+//     }
 
-}
+// }
 
-async function showWeather2()
-{
-    try{
-        const lat = 20;
-        const lon = 80;
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`);
-        const data = await response.json();
-        console.log("Wather data -> ", data);
+// async function showWeather2()
+// {
+//     try{
+//         const lat = 20;
+//         const lon = 80;
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`);
+//         const data = await response.json();
+//         console.log("Wather data -> ", data);
 
-        // let newPara=document.createElement('p');
-        // newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+//         // let newPara=document.createElement('p');
+//         // newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
         
-        // document.body.appendChild(newPara);
-        renderWeatherInfo(data);
-    }
-    catch(err){
-        console.log("Error : ", err);
-    }
+//         // document.body.appendChild(newPara);
+//         renderWeatherInfo(data);
+//     }
+//     catch(err){
+//         console.log("Error : ", err);
+//     }
 
-}
+// }
 
-function renderWeatherInfo(data)
-{
-        let newPara=document.createElement('p');
-        newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+// function renderWeatherInfo(data)
+// {
+//         let newPara=document.createElement('p');
+//         newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
         
-        document.body.appendChild(newPara);
+//         document.body.appendChild(newPara);
 
-}
+// }
