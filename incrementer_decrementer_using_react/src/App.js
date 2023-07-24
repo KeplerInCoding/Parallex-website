bdff78cd8244fe5e5a1b0b7bcd71e2197a0d8ec2
KeplerInCoding <shreyakv8@gmail.com>
2023-07-24 21:34:59 +0530

    .

--- incrementer_decrementer_using_react/src/App.js
+++ incrementer_decrementer_using_react/src/App.js
@@ -1,12 +1,40 @@
-import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increaseHandler(){
+    setCount(count+1);
+  }
+
+  function decreaseHandler(){
+    setCount(count-1);
+
+  }
+
+  function resetHandler(){
+    setCount(0);
+
+  }
+
+
+
+
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-teal-300 to-green-200 gap-10">
+      <h1 className="text-black text-[30px] font-extrabold">INCREMENTER AND DECREMENTER</h1>
+
+      <div className="flex border-4 border-green-900 bg-green-900/20">
+        <button onClick={increaseHandler} className="text-black text-[30px] font-extrabold w-16 border-r-2 m-2 border-green-900 hover:text-black/50">+</button>
+        <div className="text-black text-center text-[30px] font-extrabold w-20 m-2 ">{count}</div>
+        <button onClick={decreaseHandler} className="text-black text-[30px] font-extrabold w-16 border-l-2 m-2 border-green-900 hover:text-black/50">-</button>
+      </div>
+
+      <button onClick={resetHandler} className="text-black text-[30px] font-extrabold hover:text-[32px] hover:text-rose-800 h-10">RESET</button>
+
+    </div>
   );
 }
 
